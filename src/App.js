7618b7b386f55e9a95bb3b052a39e83641cadc4d
Nitesh <niteshchandrakar2@gmail.com>
2023-08-23 21:30:46 +0530

    fw25_034

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Home } from './Pages/Home';
+
+
 function App() {
   return (
     <div className="App">
-     CredScore
+<Home/>
     </div>
   );
 }
