cfd693a4c4b98ec5e77a19d179cf08e16db1c812
Sonika2223Kanojia <sonikakanojia534@gmail.com>
2023-08-27 15:57:02 +0530

    update nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,60 +1,86 @@
 import { Link } from "react-router-dom";
-import { Image , HStack, Box, Button} from '@chakra-ui/react';
-import  logo from '../Images/project logo (1).png'
-const  Navbar =()=>{
+import { Image, HStack, Box, Button, Divider } from '@chakra-ui/react';
+import logo from '../Images/logo.ico'
+import { useState } from "react";
 
-    return (
-        <>
+const Navbar = () => {
 
-          <HStack width="100%" height="90px">
-            <Box width="20%"  display="flex"
-            justifyContent="center" >
-              <Link to="/">
-          <Box width="200px"  >
-            <Image src={logo} width="100%"  />
-          </Box>
-              </Link>
+  const bcolor = "#adf7d8"
+  const color = "#34eb9b"
+
+  return (
+    <>
+
+      <HStack 
+        maxwidth="100%" height="50px"
+      >
+        <Box width="25%" display="flex" 
+          justifyContent="left" alignItems="center">
+          <Link to="/">
+            <Box width="200px" alignItems="center" paddingTop="10px" >
+              <Image src={logo} width="100%" height="100%" />
             </Box>
-            <Box width="40%"  display="flex"
-            justifyContent="center"
-            alignItems="center"
-             fontSize="18px"
-             fontWeight="600"
-          >
+          </Link>
+        </Box>
+        <Box width="50%" display="flex"
+          justifyContent="space-evenly"
+          alignItems="center"
+          fontSize="18px"
+          fontWeight="600"
+        >
 
-          
-          <Link to="home">
-            Home
+
+          <Link to="home"
+          >
+            <Button width="100%" padding="8px 15px"
+              border="none" borderRadius="30px"
+              color="black"
+              backgroundColor={bcolor}>Home</Button>
           </Link>
           <Link to="info">
-            CreditReport
+
+            <Button width="100%" padding="8px 15px"
+              border="none" borderRadius="30px"
+              color="black"
+              backgroundColor={bcolor}> CreditReport</Button>
+          </Link>
+          <Link to="info">
+
+            <Button width="100%" padding="8px 15px"
+              border="none" borderRadius="30px"
+              color="black"
+              backgroundColor={bcolor}>  CreditEducation</Button>
           </Link>
           <Link to="blog">
-            Blog
+
+            <Button width="100%" padding="8px 15px"
+              border="none" borderRadius="30px"
+              color="black"
+              backgroundColor={bcolor}>Blog</Button>
           </Link>
-            </Box>
-            <Box width="40%"  display="flex"
-            justifyContent="center"
-            alignItems="center"
-          
-            >
-           <Box width="70%" 
-              fontSize="18px"
-              fontWeight="600"
-             display="flex"
-             alignItems="center"
-              justifyContent="space-around">
-          <Link to="/" >
-            Contact Us
-          </Link>
-           <Button width="50%" height="30px"  backgroundColor="#01c087">Demo</Button>
-           </Box>
-            </Box>
+        </Box>
+        <Box width="25%" display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100%"
+        >
 
 
-          </HStack>
-        </>
-    )
+          <Button width="20%" height="100%" backgroundColor={bcolor}
+            marginRight="10px" border="none"  >Log In</Button>
+
+          <Button width="20%" height="100%"
+            border="none"
+            backgroundColor={bcolor}>Register</Button>
+
+        </Box>
+
+      </HStack >
+      <Divider orientation='horizontal' color={color} />
+
+      
+    </>
+  )
 }
 
 export default Navbar;
